2322114f9d555ed0a195b4c21f5fe5cb4b35a916
Nataliia Pidbereznia <86125371+pidbereznia@users.noreply.github.com>
2021-07-22 20:32:03 -0400

    add wrappers for api tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,16 +1,26 @@
 import {expect} from 'chai';
-import supertest from 'supertest';
+import AuthHelper from '../helper/auth.helper'
+
+
 
 describe('auth', function () {
-    let result;
+    const credentials = {
+        valid: {
+            login: process.env.LOGIN,
+            password: process.env.PASSWORD
+        },
+        invalid: {
+            login: 'invalid',
+            password: 'invalid'
+        }
+    }
 
 
     describe('successful log in', function () {
+        const authHelper = new AuthHelper();
 
-        before(function () {
-            result = supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login: process.env.LOGIN, password: process.env.PASSWORD});
+        before(async function () {
+            await authHelper.post(credentials.valid.login, credentials.valid.password);
         });
 
         it('response status code is 200 ', function () {
@@ -21,48 +31,43 @@ describe('auth', function () {
         //             expect(res.statusCode).to.eq(200);
         //
         //         });
-           result.expect(200);
+
+            expect (authHelper.response.statusCode).to.eq(200);
+
         });
 
         it('response body contains authorization token', function () {
-            // request
-            //     .post('/auth')
-            //     .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-            //     .end(function (err, res) {
-            //
-            //         expect(res.body.token).not.to.be.undefined;
-            //
-            //
-            //     });
-            result.end(function (err, res){
-                expect(res.body.token).not.to.be.undefined;
+             // request
+             //     .post('/auth')
+             //     .send({login: process.env.LOGIN, password: process.env.PASSWORD})
+             //     .end(function (err, res) {
+             //
+             //         expect(res.body.token).not.to.be.undefined;
+
+
+                // });
+               expect(authHelper.response.body.token).not.to.be.undefined;
+
 
             });
         });
 
-    });
-    describe('log in with wrong credential should return error', function (){
+   describe('log in with wrong credential should return error', function (){
+       const authHelper = new AuthHelper();
+             before(async function (){
 
-        before(function (){
+                 await authHelper.post(credentials.invalid.login, credentials.invalid.password);
+         });
 
-            result = supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login: 'wrong', password: 'wrong'});
-        });
+           it('response status code is 404', function (){
+               expect (authHelper.response.statusCode).to.eq(404);
 
-        it('response status code is 404', function (){
-            result.expect(404);
+           });
 
-        });
+         it('response body contains error message', function (){
+             expect(authHelper.response.body.message).to.eq('Wrong login or password.');
 
-        it('response body contains error message', function (){
-
-            result.end(function (err, res){
-
-                expect(res.body.message).to.eq('Wrong login or password.');
-
-
-                });
-        });
-    });
+           });
+   });
 });
+
